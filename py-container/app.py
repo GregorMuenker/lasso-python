344c8dde54c58894f5eb2905ec99630267699945
Gregor Münker <gregormuenker@googlemail.com>
2024-04-02 17:11:34 +0200

    flassk server

--- py-container/app.py
+++ py-container/app.py
@@ -1,24 +1,25 @@
-import pysolr
-import os
+# app.py
+from flask import Flask, request, jsonify
 
-if 'DOCKER' not in os.environ:
-    from dotenv import load_dotenv
-    load_dotenv()
+app = Flask(__name__)
 
-# Create a client instance.
-solr_path = os.getenv("SOLR_PATH")
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    from upload import test_docs
+    test_docs()
+    return jsonify({'message': 'File uploaded successfully'})
 
-solr = pysolr.Solr(solr_path + 'lasso_quickstart', timeout=10)
+@app.route('/docs', methods=['GET'])
+def get_document():
+    name = request.args.get('name')
+    from getdoc import get_solr_documents_by_name
+    return jsonify({'documents': get_solr_documents_by_name(name)})
 
-docs = [
-    {
-        "id": "doc_1",
-        "title": "A test document",
-    },
-    {
-        "id": "doc_2",
-        "title": "The Banana: Tasty or Dangerous?",
-    },
-]
+@app.route('/fetch', methods=['GET'])
+def fetch_data():
+    # Handle data fetching logic here
+    # Example: Fetch data from a database
+    return jsonify({'data': 'Some fetched data'})
 
-solr.add(docs)
+if __name__ == '__main__':
+    app.run(debug=True, port=8000)
