060ff082ae5acfa636ce20ab60f2983d411b1d02
NWichter-NeoTube <60216462+NWichter@users.noreply.github.com>
2024-07-29 23:35:11 +0200

    more files

--- routes/solr_routes.py
+++ routes/solr_routes.py
@@ -2,4 +2,32 @@
 
 from fastapi import APIRouter
 
-router = APIRouter(tags=["solr"])
+router = APIRouter(prefix="solr/", tags=["solr"])
+
+
+# check solr health
+
+
+@router.post("/testupload")
+def testupload() -> dict:
+    return {"message": "Test upload route"}
+
+
+@router.get("/testdocs")
+def testdocs() -> dict:
+    return {"message": "Test docs route"}
+
+
+@router.get("/docs")
+def get_document() -> dict:
+    return {"message": "Get document route"}
+
+
+@router.post("/add")
+def upload_file() -> dict:
+    return {"message": "Upload file route"}
+
+
+@router.get("/fetch")
+def fetch_data():
+    return {"message": "Fetch data route"}
