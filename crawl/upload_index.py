df1c2d7fa749b8da3c95a2b4e43f66f43f4a2671
maxlautenbach <mx@unity-mail.de>
2024-08-04 14:44:50 +0200

    New Project Structure

--- crawl/upload_index.py
+++
@@ -1,5 +0,0 @@
-import requests
-
-
-def upload_index(index):
-    requests.post('http://localhost:8983/solr/lasso_quickstart/update/json/docs', json=index)
--- type-inference/temp.py
+++
@@ -1,2 +0,0 @@
-def add(el1: tuple, el2: str):
-    return el1 + el2
