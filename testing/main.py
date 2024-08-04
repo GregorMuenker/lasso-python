d22c72fccf01e7182f138493240fe9e4f964bec3
maxlautenbach <mx@unity-mail.de>
2024-08-04 15:26:36 +0200

    Structure Prep

---
+++ testing/main.py
@@ -0,0 +1,14 @@
+import sys
+from crawl import install, run, splitting, upload_index
+
+
+def index_package(name):
+    install.install(name)
+    run.move_active(name)
+    index = splitting.get_module_index(name)
+    upload_index.upload_index(index)
+    run.remove_active()
+
+
+if __name__ == "__main__":
+    index_package("urllib3")
