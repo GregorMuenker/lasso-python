cc3e484d95bf4a2ac24806813312d652e51be64c
Florian Ruehle <florianruehle@web.de>
2024-09-11 21:17:53 +0200

    Evaluation WIP

--- backend/arena/evaluation.py
+++ backend/arena/evaluation.py
@@ -1,59 +1,86 @@
-import json
-from module_parser import parse_code
 from adaptation_identification import AdaptationHandler
+from execution import execute_test, ExecutionEnvironment
 from lql.antlr_parser import parse_interface_spec
-from execution import ExecutionEnvironment, execute_test
+from solr_parser import parse_solr_response
+import git
+import sys
+
+repo = git.Repo(search_parent_directories=True)
+sys.path.insert(0, repo.working_tree_dir)
+
+from backend.crawl import import_helper
+from backend.crawl.nexus import Nexus, Package
+from backend.arena.lasso_solr_connector import LassoSolrConnector
 from sequence_specification import SequenceSpecification
-import os
+from ignite import LassoIgniteClient
 
-file = open('evaluation_sanitized-mbpp.json', 'r')
-data = json.load(file)  
-
-tasks = [0, 1]
-code = ""
-for task in tasks:
-    code += data[task]["code"] + "\n\n"
-
-file = open('evaluation_output.py', 'w')
-file.write(code)
-file = open('evaluation_output.py', 'r')
-file_content = file.read()
-module_under_test = parse_code(file_content, "MBPP")
-
-lql_string = """
-    Benchmark {
-        not_prime(int)->bool
-        similar(Any, Any)->set
+if __name__ == "__main__":
+    lql_string = """
+    Task2 {
+        similar_elements(tuple, tuple)->set
     }
     """
 
-interface_spec = parse_interface_spec(lql_string)
-sequence_spec = SequenceSpecification("evaluation.xlsx")
+    interfaceSpecification = parse_interface_spec(lql_string)
 
-adaptationHandler = AdaptationHandler(
-    interface_spec,
-    module_under_test,
-    maxParamPermutationTries=1,
-    onlyKeepTopNMappings=3,
-)
-adaptationHandler.identifyAdaptations()
-adaptationHandler.identifyConstructorAdaptations()
-adaptationHandler.visualizeAdaptations()
-adaptationHandler.generateMappings()
+    sequenceSpecification = SequenceSpecification("./evaluation_sheets/2.xlsx")
 
-executionEnvironment = ExecutionEnvironment(
-    adaptationHandler.mappings,
-    sequence_spec,
-    interface_spec,
-)
+    solr_url = "http://localhost:8983/solr/lasso_quickstart"
+    solr_conn = LassoSolrConnector(solr_url)
 
-execute_test(
-    executionEnvironment,
-    adaptationHandler,
-    module_under_test.moduleName,
-    import_from_file_path="evaluation_output.py"
-)
+    allModulesUnderTest, required_packages = solr_conn.generate_modules_under_test(interfaceSpecification)
 
-executionEnvironment.printResults()
+    print(required_packages)
 
-os.remove("evaluation_output.py")
+    imp_helper = import_helper.ImportHelper(runtime=True)
+    nexus = Nexus()
+    for package in required_packages:
+        package_name, version = package.split("==")
+        pkg = Package(package_name, version)
+        nexus.download(pkg)
+        # imp_helper.pre_load_package(package_name, version)
+        # dependencies = import_helper.get_dependencies(package_name, version)
+        # for dep_name in dependencies:
+        #     dep_version = dependencies[dep_name]['version']
+        #     imp_helper.pre_load_package(dep_name, dep_version)
+
+
+    # # Setup Ignite client
+    # lassoIgniteClient = LassoIgniteClient()
+    
+    # # Iterate through all modules under test
+    # for moduleUnderTest in allModulesUnderTest:
+    #     adaptationHandler = AdaptationHandler(
+    #         interfaceSpecification,
+    #         moduleUnderTest,
+    #         maxParamPermutationTries=2,
+    #         onlyKeepTopNMappings=10,
+    #     )
+    #     adaptationHandler.identifyAdaptations()
+    #     adaptationHandler.identifyConstructorAdaptations()
+    #     adaptationHandler.visualizeAdaptations()
+    #     adaptationHandler.generateMappings()
+
+    #     executionEnvironment = ExecutionEnvironment(
+    #         adaptationHandler.mappings,
+    #         sequenceSpecification,
+    #         interfaceSpecification,
+    #         recordMetrics=True,
+    #     )
+
+    #     execute_test(
+    #         executionEnvironment,
+    #         adaptationHandler,
+    #         moduleUnderTest.moduleName,
+    #         import_from_file_path = path,
+    #     )
+
+    #     executionEnvironment.printResults()
+    #     executionEnvironment.saveResults(lassoIgniteClient)
+    
+
+    # df = lassoIgniteClient.getDataFrame()
+    # print(df)
+
+    # lassoIgniteClient.cache.destroy()
+    # lassoIgniteClient.client.close()
