aef2cdab5da4e01af21bd6df1f4e4a57daf16ecb
GregorMuenker1 <gregor.muenker@sap.com>
2024-09-15 14:06:14 +0200

    indexjson fix

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 sh lassoindex_setup.sh
+echo {} > backend/crawl/index.json
 docker-compose -f docker-compose.yaml up -d
 docker cp solr/data/lasso_python/conf/managed-schema.xml lasso_solr_quickstart:/var/solr/data/lasso_python/conf/managed-schema.xml
 # Wait for the container to be ready
