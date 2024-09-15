1355a46b1adba34cd4985080065d63276b953b50
Aaron Koßler <aaron.kossler@googlemail.com>
2024-09-15 19:52:19 +0000

    Final modification.

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 # sh lassoindex_setup.sh
 echo {} > backend/crawl/index.json
-docker-compose -f docker-compose.yaml up -d
-# docker compose -f docker-compose.yaml up -d
+# docker-compose -f docker-compose.yaml up -d
+docker compose -f docker-compose.yaml up -d
 docker cp solr/data/lasso_python/conf/managed-schema.xml lasso_solr_quickstart:/var/solr/data/lasso_python/conf/managed-schema.xml
 # Wait for the container to be ready
 sleep 120
--- setup_dev.sh
+++ setup_dev.sh
@@ -3,8 +3,8 @@ pip install -r ./backend/arena/requirements.txt
 pip install GitPython jupyter streamlit
 sh lassoindex_setup.sh
 echo {} > backend/crawl/index.json
-docker-compose -f docker-compose-dev.yaml up -d
-# docker compose -f docker-compose-dev.yaml up -d
+# docker-compose -f docker-compose-dev.yaml up -d
+docker compose -f docker-compose-dev.yaml up -d
 docker cp solr/data/lasso_python/conf/managed-schema.xml lasso_solr_quickstart:/var/solr/data/lasso_python/conf/managed-schema.xml
 # Wait for the container to be ready
 sleep 120
