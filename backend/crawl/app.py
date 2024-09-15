120e9d6b83996eec70481352e18374f813591243
GregorMuenker1 <gregor.muenker@sap.com>
2024-09-15 15:34:33 +0200

    crawl param

--- backend/crawl/app.py
+++ backend/crawl/app.py
@@ -1,14 +1,14 @@
 """app.py - This file contains REST API."""
 
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from crawl_pipeline import index_package
 
 app = FastAPI()
 
 @app.post("/crawl/{package_name}")
-def crawl(package_name: str, type_inference: str = None):
-    type_inference_param = type_inference == "True"
+def crawl(package_name: str, request: Request):
+    request.query_params.get('type_inference_param', "None")
     return index_package(package_name, type_inference_param)
 
 @app.get("/health")
