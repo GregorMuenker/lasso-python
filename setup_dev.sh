e8265ba1ec5d0ee74360f9ce68bf0140154a37a3
Aaron Koßler <aaron.kossler@googlemail.com>
2024-09-15 15:38:48 +0000

    Modified setup scripts.

--- setup_dev.sh
+++ setup_dev.sh
@@ -2,7 +2,9 @@ pip install -r ./backend/crawl/requirements.txt
 pip install -r ./backend/arena/requirements.txt
 pip install GitPython
 sh lassoindex_setup.sh
+echo {} > backend/crawl/index.json
 docker-compose -f docker-compose-dev.yaml up -d
+# docker compose -f docker-compose-dev.yaml up -d
 docker cp solr/data/lasso_python/conf/managed-schema.xml lasso_solr_quickstart:/var/solr/data/lasso_python/conf/managed-schema.xml
 # Wait for the container to be ready
 sleep 120
